820b19036f35ae30d6bfe2cecb29a7277b8cc298
Luis Hernández <luis.hernandez.solis@correounivalle.edu.co>
2025-08-07 22:56:37 -0500

    UPDATE: CV

---
+++ src/env.d.ts
@@ -0,0 +1,9 @@
+interface ImportMetaEnv {
+  readonly EMAILJS_SERVICE_ID: string;
+  readonly EMAILJS_TEMPLATE_ID: string;
+  readonly EMAILJS_USER_ID: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
